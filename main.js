89eb65f4478ca32c401046b455cd0e7cabcfd667
robloxmuskateers <79553090+robloxmuskateers@users.noreply.github.com>
2021-10-13 17:10:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {
+}
+
+function setup() {
+    //create and center canvas
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function takeSnapshot() {
+    save('myMustache.png');
+}
